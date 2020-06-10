24779b5d0a7764c0b63b8b987095c542583fae72
Joe Butler <profjb58@users.noreply.github>
2020-06-10 18:04:55 +0100

    Initial commit

---
+++ src/main/java/io/github/profjb58/metropolis/Metropolis.java
@@ -0,0 +1,29 @@
+package io.github.profjb58.metropolis;
+
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Metropolis.MOD_ID)
+public class Metropolis
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "metropolis";
+
+    public Metropolis() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) { }
+
+    private void doClientStuff(final FMLClientSetupEvent event) { }
+}
